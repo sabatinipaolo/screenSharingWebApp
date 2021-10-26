23acc86cde7e26028f0d2c95fafdaf723320fee9
Paolo Proff. Sabatini <7931893-paolo.sabatini@users.noreply.gitlab.com>
2021-10-26 22:00:17 +0200

    rimosse le stanze per semplicità

--- myWebRTC/index.js
+++ myWebRTC/index.js
@@ -6,6 +6,7 @@ const nodeStatic = require('node-static');
 
 const { Server } = require("socket.io");
 var fileServer = new(nodeStatic.Server)();
+
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 });
@@ -14,45 +15,31 @@ console.log('listening on http://localhost:8080');
 const io = new Server(app);
 app.listen(8080);
 
-
 var staCondividendo="NESSUNO";
-
- 
 
 io.on('connection', (socket) => {
   console.log('a user connected ');
-
-
+  let clients = io.sockets.sockets;// is a Map
+  io.sockets.emit('lista_stanza',Array.from(clients.keys()).join(' '));
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
-  socket.on('join', (room) => {
-    
-    socket.join(room);
-    
-    var clientsInRoom = io.sockets.adapter.rooms.get(room);
-    var numClients = clientsInRoom.size
-    
-    io.to(room).emit('lista_stanza',Array.from(clientsInRoom).join(' '));
-   
-  });
-  
-  socket.on('condivido', (room) => {
+  socket.on('condivido', () => {
     if (staCondividendo == "NESSUNO"){
       staCondividendo=socket.id;
-      io.to(room).emit('sta_condividendo', staCondividendo);
+      io.sockets.emit('sta_condividendo', staCondividendo);
     }
-   
+
   });
   
-  
-  socket.on('stop_condivido', (room) => {
+  socket.on('stop_condivido', () => {
+    console.log("stop condivido");
     if (staCondividendo != "NESSUNO"){
       if ( staCondividendo == socket.id ) {
             staCondividendo="NESSUNO";
-            io.to(room).emit('stop_condividendo');
+            io.sockets.emit('stop_condividendo');
       }
     }
   });
--- myWebRTC/js/main.js
+++ myWebRTC/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var pcConfig = {
   'iceServers': [] ///QUI !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 };
@@ -12,14 +11,10 @@ const partecipanti = document.getElementById('partecipanti');
 const condivisore = document.getElementById('condivisore');
 const chiSonoIo = document.getElementById('chiSonoIo');
 
-
-
-var room="LAB";
-
+//var room="LAB";
 
 var socket = io.connect();
-socket.emit('join',room);
-
+//socket.emit('join',room);
 
 socket.on("connect", () => { 
   chiSonoIo.innerHTML=socket.id;
@@ -32,25 +27,18 @@ socket.on('lista_stanza', function (lista) {
 
 socket.on('sta_condividendo', function (staCondividendo) {
   condivisore.innerHTML = staCondividendo;
-
 });
 
 socket.on('stop_condividendo', function (staCondividendo) {
   condivisore.innerHTML = "";
-
 });
 
-
 startButton.onclick = function() {
-       socket.emit('condivido',room);
-       
-
-
+       socket.emit('condivido');
+       console.log("CONDIVIDO CLICK");
 }
 
 stopButton.onclick = function() {
-
-       socket.emit('stop_condivido',room);
-  
-
+       socket.emit('stop_condivido');
+         console.log("STOP CONDIVIDO CLICK");
 }
